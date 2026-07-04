6a9cadc174175cd7dd7c8ee3b243a823707e814a
MateuszRumin <rumin880@gmail.com>
2026-07-04 18:44:17 +0200

    cgsg

--- select/main.go
+++ select/main.go
@@ -22,7 +22,7 @@ func testEntry() {
 		fmt.Println("Połączenie z bazą danych działa poprawnie.")
 		exp := Experiment{
 			Name:    "Np Select Posts",
-			Queries: comments,
+			Queries: postH,
 			Runs:    1,
 		}
 
